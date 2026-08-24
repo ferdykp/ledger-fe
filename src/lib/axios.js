19bb9b2773d7cc2c68cc506ed9557973c92162a2
ferdy <kpferdy@gmail.com>
2026-08-24 22:23:14 +0700

    add .env

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
+// ledger-web/src/lib/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
     Accept: "application/json",
