4d4a120f7f9fdcf5e126798d29862a66c60fba71
ferdy <kpferdy@gmail.com>
2026-08-24 23:46:59 +0700

    pws

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import { registerSW } from "virtual:pwa-register";
+
+// Registrasi Service Worker PWA secara otomatis
+registerSW({ immediate: true });
 
 const app = createApp(App);
 
