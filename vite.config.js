4cce2646b0cdfdaf45487cd9ebbde2a7554682be
ferdy <kpferdy@gmail.com>
2026-08-25 00:03:19 +0700

    refresh update

--- vite.config.js
+++ vite.config.js
@@ -46,6 +46,8 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        skipWaiting: true, // <--- Paksa Service Worker baru langsung aktif
+        clientsClaim: true,
       },
     }),
   ],
