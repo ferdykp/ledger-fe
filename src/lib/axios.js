4d4ca4d35413ab91d2c852ba712b38b2787787a1
ferdy <kpferdy@gmail.com>
2026-08-24 23:22:54 +0700

    add icon public

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
+  withCredentials: true, // Tambahkan ini jika pakai Laravel Sanctum SPA
   headers: {
     "X-Requested-With": "XMLHttpRequest",
     Accept: "application/json",
